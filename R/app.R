75d64d976a3136fd964f4760a1389ff99b4aee01
emilopezcano <emilopezcano@gmail.com>
2026-03-10 14:42:24 +0100

    unse namespace and jpackage_doc

---
+++ R/app.R
@@ -0,0 +1,45 @@
+library(shiny)  
+library(bslib)
+library(reactable)
+library(ggplot2)
+library(dplyr)
+library(lubridate)
+resultados <- read.csv("data/results.csv")
+seleccion_ui <- function(id) {
+  ns <- NS(id) # Crea un espacio de nombres para el módulo
+  tagList(
+    selectizeInput(ns("pais"), "Selecciona un país:", choices = unique(resultados$home_team)),
+    verbatimTextOutput(ns("salida"))
+  )
+}
+seleccion_server <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    output$salida <- renderPrint({
+      paste("Selección:", input$pais)
+    })
+    return(list(pais = reactive(input$pais))) # Devuelve el país seleccionado como un valor reactivo
+  })
+}
+# App principal
+ui <- bslib::page_fluid(
+    bslib::navset_card_underline(
+        nav_panel("Panel 1", seleccion_ui("mod1"),
+        plotOutput("plot1")), # Llama al módulo en la UI
+        nav_panel("Panel 2", seleccion_ui("mod2"),
+        reactableOutput("tabla2"))  # Llama al
+    )
+)
+server <- function(input, output, session) {
+  pais1 <- seleccion_server("mod1") # Llama al módulo en el server
+  pais2 <- seleccion_server("mod2") # Llama al módulo otra vez para la segunda instancia
+  output$plot1 <- renderPlot({
+    # Código para generar el gráfico usando pais1$pais()
+    resultados |> filter(home_team == pais1$pais()) |> 
+      group_by(year = year(as.Date(date))) |> summarise(home_score = sum(home_score)) |> ggplot(aes(x = year, y = home_score)) + geom_line()
+  })
+  output$tabla2 <- renderReactable({
+    # Código para generar la tabla usando pais2$pais()
+    resultados |> filter(home_team == pais2$pais()) |> reactable()
+  })
+}
+shinyApp(ui, server)
--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # musavcddpkg
 
-Example package for a shiny app
+Proceso de creación de este paquete tras crearlo vacío en GitHub:
+
+1. Crear carpeta R
+
+2. Crear archivo DESCRIPTION
+
+    usethis::use_description()
+
+3. Crear archivo NAMESPACE
+
+    usethis::use_namespace()
+
+
+4. Añade archivo de documentación del paquete.
+
+    usethis::use_package_doc()
